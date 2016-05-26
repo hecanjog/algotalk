5984d6d336214c892fdec1371209d5caa5ab3399
Erik Schoster <erik@hecanjog.com>
2016-05-26 17:05:50 -0500

    Last minute tweaking

--- examples/basic.py
+++ examples/basic.py
@@ -1,5 +1,5 @@
 from pippi import dsp, tune
-from hcj import keys, data, Tracks, snds, curves
+from hcj import keys, data, Tracks, snds, curves, fx
 
 midi = {'mpk': 'MPKmini2 MIDI 1'}
 trigger = {
@@ -7,79 +7,13 @@ trigger = {
     'notes': range(127)
 }
 
-def makeOnsets(length, log, wobble, div, num_beats, offset=False):
-    if offset:
-        offset = dsp.randint(0, 3)
-    else:
-        offset = 0
-
-    pattern = dsp.eu(num_beats, dsp.randint(1, num_beats/div), offset)
-    dsp.log(pattern)
-
-    if wobble:
-        points = [ dsp.mstf(100, 500) for _ in range(dsp.randint(2, 8)) ]
-        plength = sum(points)
-        mult = length / float(plength)
-        onsets = curves.bezier(points, num_beats)
-        onsets = [ int(o * mult) for o in onsets ]
-
-    else:
-        beat = float(length) / num_beats
-        num_beats = length / beat
-        beat = int(round(beat))
-        onsets = [ beat for _ in range(int(round(num_beats))) ]
-
-    return onsets
-
-def makeKicks(length, log, wobble):
-    out = Tracks()
-    onsets = makeOnsets(length, log, wobble, 2, 4)
-
-    k = log.choose(snds.search('mc303/*kick*'))
-
-    for onset in onsets:
-        out.add(k, onset)
-
-    out = out.mix()
-
-    return (dsp.fill(out, length, silence=False), log)
-
-def makeHats(length, log, wobble):
-    out = Tracks()
-    onsets = makeOnsets(length, log, wobble, 1, 16)
-
-    h = log.choose(snds.search('mc303/*hat*'))
-
-    for onset in onsets:
-        out.add(h, onset)
-
-    out = out.mix()
-
-    return (dsp.fill(out, length, silence=False), log)
-
-def makeClaps(length, log, wobble):
-    out = Tracks()
-    onsets = makeOnsets(length, log, wobble, 2, 8, True)
-
-    c = log.choose(snds.search('mc303/*clap*'))
-
-    for onset in onsets:
-        out.add(c, onset)
-
-    out = out.mix()
-
-    dsp.log(length)
-    dsp.log(sum(onsets))
-
-    return (dsp.fill(out, length, silence=False), log)
-
 def play(ctl):
     p = ctl.get('param')
     key = p.get('key', 'c')
     mpk = ctl.get('midi').get('mpk')
     bpm = p.get('bpm', 100)
     beat = dsp.bpm2frames(bpm)
-    length = beat * 8
+    length = beat * 2 * mpk.geti(6, low=1, high=6)
 
     gamut = mpk.geti(20)
     poly = mpk.geti(21)
@@ -89,24 +23,102 @@ def play(ctl):
     perc = mpk.get(25)
     wobble = mpk.get(26)
 
+    min_octave = mpk.geti(4, low=1, high=4)
+    max_octave = mpk.geti(4, low=3, high=7)
+    octave = dsp.randint(min_octave, max_octave)
+
+    k = dsp.randchoose(snds.search('mc303/*kick*'))
+    h = dsp.randchoose(snds.search('mc303/*hat*'))
+    c = dsp.randchoose(snds.search('mc303/*clap*'))
+
+    pointer = p.get('pointer', default=0)
+    log = data.Logistic(r=dsp.rand(3.9, 3.99), x=0.5, size=1024, pointer=pointer)
+    
     if fixed:
         seed = mpk.get(5)
         dsp.seed(str(seed))
 
+    def makeOnsets(length, wobble, div, num_beats, offset=False):
+        if offset:
+            offset = dsp.randint(0, 3)
+        else:
+            offset = 0
+
+        pattern = dsp.eu(num_beats, dsp.randint(1, num_beats/div), offset)
+        dsp.log(pattern)
+
+        if wobble:
+            points = [ dsp.mstf(100, 500) for _ in range(dsp.randint(2, 8)) ]
+            plength = sum(points)
+            mult = length / float(plength)
+            onsets = curves.bezier(points, num_beats)
+            onsets = [ int(o * mult) for o in onsets ]
+
+        else:
+            beat = float(length) / num_beats
+            num_beats = length / beat
+            beat = int(round(beat))
+            onsets = [ beat * i for i in range(int(round(num_beats))) ]
+
+        return onsets
+
+
+    def makeKicks(length, wobble):
+        out = Tracks()
+        onsets = makeOnsets(length, wobble, 2, 4, True)
+        kick = dsp.taper(k, 20)
+        kick = dsp.transpose(kick, dsp.rand(1, 2))
+
+        for onset in onsets:
+            out.add(kick, onset)
+
+        out = out.mix()
+
+        out = dsp.fill(out, length, silence=False)
+        return dsp.taper(out, 40)
+
+
+    def makeHats(length, wobble):
+        out = Tracks()
+        onsets = makeOnsets(length, wobble, 1, 16, True)
+
+        for onset in onsets:
+            out.add(h, onset)
+
+        out = out.mix()
+        out = dsp.split(out, dsp.flen(out) / dsp.randchoose([2, 3, 4]))
+        out = dsp.randshuffle(out)
+        out = ''.join(out)
+        out = dsp.fill(out, length, silence=False)
+        return dsp.taper(out, 40)
+
+
+    def makeClaps(length, wobble):
+        out = Tracks()
+        onsets = makeOnsets(length, wobble, 2, 8, True)
+
+        for onset in onsets:
+            clap = dsp.stretch(c, int(dsp.flen(c) * log.get(1, 10)), grain_size=dsp.randint(20, 120))
+            out.add(clap, onset)
+
+        out = out.mix()
+        out = dsp.split(out, dsp.flen(out) / dsp.randchoose([2, 3, 4]))
+        out = [ dsp.taper(o, 20) for o in out ]
+        out = dsp.randshuffle(out)
+        out = ''.join(out)
+        out = dsp.fill(out, length, silence=False)
+        return dsp.taper(out, 40)
+
+
     if mpk.get(8) == 0:
         return dsp.pad('', 0, dsp.mstf(100))
 
-    pointer = p.get('pointer', default=0)
-    log = data.Logistic(r=dsp.rand(3.9, 3.99), x=0.5, size=1024, pointer=pointer)
-
     def randomScale():
-        octave = dsp.randint(3, 6)
         scale = [1,2,3,4,5,6,7,8]
         freqs = tune.fromdegrees(scale, octave=octave, root=key)
         return freqs
 
     def gamutScale():
-        octave = dsp.randint(3, 6)
         num_notes = dsp.randint(3, 6)
         scale = [ dsp.randchoose([1,2,3,4,5,6,7,8]) for _ in range(num_notes) ]
         freqs = tune.fromdegrees(scale, octave=octave, root=key)
@@ -164,17 +176,19 @@ def play(ctl):
         layers += [ layer ]
 
     out = dsp.mix(layers)
+    out = dsp.amp(out, 0.75)
     out = dsp.fill(out, length)
-    out = dsp.pad(out, 0, int(dsp.stf(0.5, 1) * mpk.get(7)))
+
+    #out = dsp.pad(out, 0, int(dsp.stf(0.5, 1) * mpk.get(7)))
 
     p.set('pointer', log.pointer)
 
-    out = dsp.amp(out, mpk.get(8))
-
     if perc:
-        kicks, log = makeKicks(dsp.flen(out), log, wobble)
-        hats, log = makeHats(dsp.flen(out), log, wobble)
-        claps, log = makeClaps(dsp.flen(out), log, wobble)
+        kicks = makeKicks(length, wobble)
+        hats = makeHats(length, wobble)
+        claps = makeClaps(length, wobble)
         out = dsp.mix([ out, kicks, hats, claps ])
+
+    out = dsp.amp(out, mpk.get(8))
 
     return out
